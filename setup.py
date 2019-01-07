e1c4e83ab62033d582885dedbec7ea003374c5a7
pohmelie <multisosnooley@gmail.com>
2019-01-07 19:39:39 +0300

    setup: more declarative description

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup
-from os.path import join, dirname
 
-setup(
-    name='easyparser',
-    version='0.1.0',
-    python_requires='>=3.6.0',
-    description='data ejector',
-    author='Andrey C, Nikita M',
-    author_email='frill2one@gmail.com',
-    packages=['easyparser'],
-    install_requires=['requests', 'lxml'],
-    long_description=open(join(dirname(__file__), 'README.md')).read()
-)
+
+setup()
